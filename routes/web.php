04e4fd51cadac31e0668ffa02fbbc0147119244e
thanhtu0910 <tuantu091005@gmail.com>
2025-11-06 19:21:53 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
 });
 
+// Registration routes
+Route::get('/register', [RegisterController::class, 'show'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+// Login / Logout routes
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
